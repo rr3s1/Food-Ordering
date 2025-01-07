2ae15e75379840f162bb6c24fe199210f408333a
aessary <raressilviulazar@gmail.com>
2025-01-07 13:51:04 +0000

    Initial Routes config

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<span>HOME PAGE</span>} />
+      <Route path="/user-profile" element={<span>USER PROFILE</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.tsx'
+import { BrowserRouter as Router }  from 'react-router-dom'
+import AppRoutes from './AppRoutes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    < Router> 
+    <AppRoutes />
+    </Router>
   </StrictMode>,
 )
