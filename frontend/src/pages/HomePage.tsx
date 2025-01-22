c3a4c75a1241c438daa0ea3569cfe464acff2256
aessary <raressilviulazar@gmail.com>
2025-01-22 09:32:28 +0000

    Responsive landing page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import landingImage from '../assets/landing.png'
+import appDownloadImage from '../assets/appDownload.png'
 
 const HomePage = () => {
   return (
@@ -8,6 +10,20 @@ const HomePage = () => {
           Tuck into a takeaway today!
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+      </div>
+      <div className='grid md:grid-cols-2 gap-5'> 
+
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Download the MernEats App for faster ordering and personalized
+            recommendations
+          </span>
+          <img src={appDownloadImage} />
+        </div>
       </div>
     </div>
   )
